dfffe658a603eeb657426a868c25b851928e808d
LoukasLecourt <73343711+LoukasLecourt@users.noreply.github.com>
2021-03-17 16:58:38 +0100

    V2.1
    Finalisation du CSS de la page CV et ajout de contenu dans contact

--- assets/js/scripts/main.js
+++ assets/js/scripts/main.js
@@ -1,6 +1,7 @@
 var navbar = document.querySelector(".navbar")
 var menu = document.querySelector(".menu")
 var menuLinks = document.querySelectorAll(".menuLink")
+var desc = document.querySelectorAll(".desc")
 
 menu.addEventListener("click", toggleHamburger)
 
@@ -13,4 +14,16 @@ menuLinks.forEach(
     function(menuLink) { 
       menuLink.addEventListener("click", toggleHamburger) 
     }
+)
+
+menu.addEventListener("click", changeOpacity)
+
+function changeOpacity(){
+    menu.classList.change("showDesc")
+    menu.classList.change("showClose")
+}
+desc.forEach(
+  function(desc) {
+    desc.addEventListener("click", changeOpacity)
+  }
 )
